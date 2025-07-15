7b426f3b552c0b29e47c71c72fac592b5c2b4f42
David Johnston <david@blacksheepcode.com>
2025-07-15 12:15:44 +1000

    Everything

---
+++ jest-example/README.md
@@ -0,0 +1,4 @@
+# Node + Jest + TypeScript + tsx boilerplate
+
+Boilerplate for running simple TypeScript + Node + Jest tests in 2024. 
+
---
+++ vitest-example/src/foo.test.ts
@@ -0,0 +1,8 @@
+
+import {describe, it, expect} from "vitest";
+
+describe('foo', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
